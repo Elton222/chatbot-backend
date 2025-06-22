dd35a562e9e82aaae3f5bc7b9c3492960013ee27
Elton222 <eltonpatson@gmail.com>
2025-06-23 02:56:37 +0300

    Update db config to use Clever Cloud credentials

--- config/db.js
+++ config/db.js
@@ -2,19 +2,23 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, // adjust as needed
+  queueLimit: 0
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('MySQL connection error:', err);
   } else {
     console.log('MySQL connected successfully');
+    connection.release(); // release back to pool
   }
 });
 
-module.exports = db;
+module.exports = pool.promise();
