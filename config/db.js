debba5a47f65ae39b4f2e54d3166657a7e0b43d0
Elton222 <eltonpatson@gmail.com>
2025-07-06 00:33:13 +0300

    Fix Supabase SSL + timeout

--- config/db.js
+++ config/db.js
@@ -5,10 +5,14 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432,
+  port: Number(process.env.DB_PORT) || 5432,
   ssl: {
-    rejectUnauthorized: false, // important for Supabase
+    rejectUnauthorized: false,
   },
+  // force IPv4 by setting this to false:
+  // this disables DNS lookup for IPv6 addresses
+  // and forces IPv4 connection attempts
+  family: 4,
 });
 
 module.exports = pool;
